0c4be22d69455d99d65b4384a050f7314acf8320
Habeeba Sinu <habeebasinu5275@gmail.com>
2026-02-10 15:14:55 +0530

    Fix Socket.IO CORS for production

--- backend/config/Socket.js
+++ backend/config/Socket.js
@@ -2,11 +2,25 @@ import { Server } from "socket.io";
 
 let io;
 
+const allowedOrigins = [
+  "http://localhost:5175",
+  "http://localhost:5179",
+  "https://service-connect-2-gn13.onrender.com"
+];
+
 export const initSocket = (server) => {
   io = new Server(server, {
     cors: {
-      origin: "http://localhost:5175",
-       methods: ["GET", "POST"],
+      origin: (origin, callback) => {
+        if (!origin) return callback(null, true);
+
+        if (allowedOrigins.includes(origin)) {
+          callback(null, true);
+        } else {
+          callback(new Error("Not allowed by CORS"));
+        }
+      },
+      methods: ["GET", "POST"],
       credentials: true
     }
   });
@@ -30,3 +44,51 @@ export const getIO = () => {
   }
   return io;
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { Server } from "socket.io";
+
+// let io;
+
+// export const initSocket = (server) => {
+//   io = new Server(server, {
+//     cors: {
+//       origin: "http://localhost:5175",
+//        methods: ["GET", "POST"],
+//       credentials: true
+//     }
+//   });
+
+//   io.on("connection", (socket) => {
+//     console.log("Socket connected:", socket.id);
+
+//     socket.on("join", (userId) => {
+//       socket.join(userId);
+//     });
+
+//     socket.on("disconnect", () => {
+//       console.log("Socket disconnected");
+//     });
+//   });
+// };
+
+// export const getIO = () => {
+//   if (!io) {
+//     throw new Error("Socket.io not initialized");
+//   }
+//   return io;
+// };
