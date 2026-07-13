4b82027a5b64843f3d9fde2234b76fa600ecd359
Habeeba Sinu <habeebasinu5275@gmail.com>
2026-07-13 20:57:51 +0530

    update Section.jsx

--- frontend/vite-project/src/pages/user/Section.jsx
+++ frontend/vite-project/src/pages/user/Section.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdOutlinePets } from "react-icons/md";
 import { TbGardenCartFilled } from "react-icons/tb";
 import { FaGasPump } from "react-icons/fa";
+import { viewallservices } from "../../api/api";
 
 function Section() {
+    const [services,setServices]=useState([])
     console.log("Section Rendered");
+    const Click=async(Category)=>{
+        const res=await viewallservices()
+         const filt=res.filter((item)=>item.service === Category)
+         setServices(filt)
+    }
   return (
     <div className="bg-white p-6 rounded-xl shadow-md m-6">
 
@@ -16,26 +23,41 @@ function Section() {
 
         <div className="flex flex-col items-center">
           <div className="bg-pink-100 p-5 rounded-full">
-            <MdOutlinePets className="text-5xl text-pink-600" />
+            <MdOutlinePets className="text-5xl text-pink-600"  onClick={() => Click("pet care")}/>
           </div>
           <p className="mt-3 font-semibold">Pet Care</p>
         </div>
 
         <div className="flex flex-col items-center">
           <div className="bg-green-100 p-5 rounded-full">
-            <TbGardenCartFilled className="text-5xl text-green-600" />
+            <TbGardenCartFilled className="text-5xl text-green-600"  onClick={() => Click("Gardening")}/>
           </div>
           <p className="mt-3 font-semibold">Gardening</p>
         </div>
 
         <div className="flex flex-col items-center">
           <div className="bg-yellow-100 p-5 rounded-full">
-            <FaGasPump className="text-5xl text-yellow-600" />
+            <FaGasPump className="text-5xl text-yellow-600"  onClick={() => Click("plumbing")}/>
           </div>
           <p className="mt-3 font-semibold">Fuel Delivery</p>
         </div>
 
       </div>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-5 mt-8">
+  {services.map((item) => (
+    <div key={item._id} className="border rounded-lg p-4 shadow">
+      <img
+        src={item.img}
+        alt={item.service}
+        className="w-full h-40 object-cover rounded"
+      />
+
+      <h2 className="font-bold mt-2">{item.companyname}</h2>
+      <p>{item.service}</p>
+      <p>₹{item.price}</p>
+    </div>
+  ))}
+</div>
 
     </div>
   );
